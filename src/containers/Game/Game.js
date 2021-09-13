b7f2ff766a645306bdc6c0708534031a1567a8b8
seaa <santas.klaus@gmail.com>
2021-09-13 14:13:30 -0300

    code comments

--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import { GameLayout } from '../../components/GameLayout';
 
+// since thre is only 8 winning combinations, we can
+// calculate this hardcoding every position
 const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
@@ -19,25 +21,40 @@ const calculateWinner = (squares) => {
       return squares[a];
     }
   }
+
+  // calculateWinner returns the value of the winner player if exists (X or O)
+  // we return null if there is no winner to rendeer an empty message on GameLayout
   return null;
 }
 
+/*
+* Main container: here we have all the logic that controls the game,
+* including our main state.
+* Note that we don't include any interface-related jsx oon containers
+*/
 const Game = () => {
 
+  // our application state.
+  // for small apps, this can be on a top-level container,
+  // but it's noot adviced for real apps, we should be using an external
+  // state handling library
   const [history, setHistory] = useState(
     [
       { squares: Array(9).fill(null) }
     ]
   );
-
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setxIsNext] = useState(true);
 
+  // clicking on a square should update the state and check for a winner
+  // we make copies of the state, in order to avoid mutations
   const handleClick = (i) => {
     const historySlice = history.slice(0, stepNumber + 1);
     const current = historySlice[historySlice.length - 1];
     const squares = current.squares.slice();
 
+    // early exit because there is a winner!
+    // we dont want to add any more steps to the history
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -51,6 +68,8 @@ const Game = () => {
     setxIsNext(!xIsNext);
   }
 
+  // moving through the history state just replaces the current step number.
+  // we dont delete the rest, since we want to be able to go Back to the Future ™
   const jumpTo = (step) => {
     setStepNumber(step);
     setxIsNext((step % 2) === 0);
